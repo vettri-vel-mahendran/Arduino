e2b400b2f57867eff1979e82098321d2b94c8248
Vettri vel Mahendran <55804805+vettri-vel-mahendran@users.noreply.github.com>
2020-08-10 20:57:07 +0530

    Add files via upload

---
+++ google_sheet_data_logger_using_IFTTT.ino
@@ -0,0 +1,106 @@
+/*
+  google sheet data logger
+
+ This example shows how to log data from esp module to google sheet using IFTTT.
+
+ created 28 Mar 2019
+ modified 28 Mar 2019
+ by Vettri vel Mahendran
+
+ */
+#ifdef ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+
+// Replace with your SSID and Password
+const char* ssid     = "******";
+const char* password = "*******";
+
+// Replace with your unique IFTTT URL resource
+const char* resource = "/trigger/node mcu data/with/key/cu46oQeSAQ1WjMmcnJc_ICJYm0jGU_IJ7XzDqEIHPPs";
+
+// How your resource variable should look like, but with your own API KEY (that API KEY below is just an example):
+//const char* resource = "/trigger/bme280_readings/with/key/nAZjOphL3d-ZO4N3k64-1A7gTlNSrxMJdmqy3";
+
+// Maker Webhooks IFTTT
+const char* server = "maker.ifttt.com";
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  initWifi();
+}
+
+void loop() {
+  makeIFTTTRequest();
+  delay(1000);
+}
+
+// Establish a Wi-Fi connection with your router
+void initWifi() {
+  Serial.print("Connecting to: ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+
+  int timeout = 10 * 4; // 10 seconds
+  while (WiFi.status() != WL_CONNECTED  && (timeout-- > 0)) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed to connect, going back to sleep");
+  }
+
+  Serial.print("WiFi connected in: ");
+  Serial.print(millis());
+  Serial.print(", IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Make an HTTP request to the IFTTT web service
+void makeIFTTTRequest() {
+  Serial.print("Connecting to ");
+  Serial.print(server);
+
+  WiFiClient client;
+  int retries = 5;
+  while (!!!client.connect(server, 80) && (retries-- > 0)) {
+    Serial.print(".");
+  }
+  Serial.println();
+  if (!!!client.connected()) {
+    Serial.println("Failed to connect...");
+  }
+
+  Serial.print("Request resource: ");
+  Serial.println(resource);
+
+  String jsonObject = String("{\"value1\":\"") + 10 + "\",\"value2\":\"" + 45
+                      + "\",\"value3\":\"" + 167 + "\"}";
+
+  client.println(String("POST ") + resource + " HTTP/1.1");
+  client.println(String("Host: ") + server);
+  client.println("Connection: close\r\nContent-Type: application/json");
+  client.print("Content-Length: ");
+  client.println(jsonObject.length());
+  client.println();
+  client.println(jsonObject);
+
+  int timeout = 5 * 10; // 5 seconds
+  while (!!!client.available() && (timeout-- > 0)) {
+    delay(100);
+  }
+  if (!!!client.available()) {
+    Serial.println("No response...");
+  }
+  while (client.available()) {
+    Serial.write(client.read());
+  }
+
+  Serial.println("\nclosing connection");
+  client.stop();
+}
