9c34f12c8b0532ee17cd6b6da163fb97c96bbc3a
Vettri vel Mahendran <55804805+vettri-vel-mahendran@users.noreply.github.com>
2020-08-10 21:38:21 +0530

    Add files via upload

---
+++ DHT11.ino
@@ -0,0 +1,34 @@
+/*
+  DHT11 temperature sensor
+
+  created 28 Mar 2019
+  modified 28 Mar 2019
+  by Vettri vel Mahendran
+
+*/
+#include <dht11.h>
+#define DHT11PIN D4
+
+dht11 DHT11;
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  Serial.println();
+
+  int chk = DHT11.read(DHT11PIN);
+
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+
+  Serial.print("Temperature (C): ");
+  Serial.println((float)DHT11.temperature, 2);
+
+  delay(3000);
+
+}
