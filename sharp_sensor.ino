6492c02de173d62d42a144e62d1b9c6bd6acc9fb
Vettri vel Mahendran <55804805+vettri-vel-mahendran@users.noreply.github.com>
2020-08-10 21:40:13 +0530

    Add files via upload

---
+++ sharp_sensor.ino
@@ -0,0 +1,33 @@
+/*
+  Distance measurement using Sharp sensor
+
+
+  created 28 Mar 2019
+  modified 28 Mar 2019
+  by Vettri vel Mahendran
+
+*/
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  int  dis = analogRead(A0);
+  Serial.print("Mean distance: ");  // returns it to the serial monitor
+  Serial.println(dis);
+
+  if (dis > 500)
+  {
+    Serial.println("object dedected");
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+
+}
