4a16a90b18333d6107db1f3830f1948418018e12
Vettri vel Mahendran <55804805+vettri-vel-mahendran@users.noreply.github.com>
2020-08-10 20:59:58 +0530

    Add files via upload

---
+++ Automatic_Terrace_plant_irrigation.ino
@@ -0,0 +1,37 @@
+/*
+  Automatic Terrace Plant Irrigation
+
+  created 28 Mar 2019
+  modified 28 Mar 2019
+  by Vettri vel Mahendran
+*/
+const int sensor_pin = A0;  /* Soil moisture sensor O/P pin */
+int relay = 2;
+
+void setup() {
+  Serial.begin(9600); /* Define baud rate for serial communication */
+  pinMode(relay , OUTPUT);
+  pinMode(A0 , INPUT);
+}
+
+void loop() {
+  int moisture;
+  int sensor_analog;
+  sensor_analog = analogRead(sensor_pin);
+  moisture = ( 73 - ( (sensor_analog / 1023.00) * 100 ) );
+  Serial.print("Moisture = ");
+  Serial.print(moisture);
+  Serial.print("%\n\n");
+
+  if ((moisture <= 0) || (moisture >= 20))
+  {
+    digitalWrite(relay, LOW);
+  }
+  if ((moisture <= 20) || (moisture >= 60))
+
+  {
+    digitalWrite(relay, HIGH);
+  }
+}
+
+
