a45ddccd07c6c3d0f32e1eb128dc27f8799354a1
Vettri vel Mahendran <55804805+vettri-vel-mahendran@users.noreply.github.com>
2020-08-10 21:07:12 +0530

    Add files via upload

---
+++ PIR_sensor.ino
@@ -0,0 +1,38 @@
+/*
+  PIR sensor
+  Passive infrared sensor interafce with arduino UNO
+
+  
+  created 28 Mar 2019
+  modified 28 Mar 2019
+  by Vettri vel Mahendran
+
+*/
+int pirPin = D7;
+int val = LOW;
+int relay = D1;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(D1, OUTPUT);
+}
+
+void loop()
+{
+  val = digitalRead(pirPin);
+  //low = no motion, high = motion
+  if (val == LOW)
+  {
+
+    Serial.println("No motion");
+
+    digitalWrite(D1, LOW);
+  }
+  else
+  {
+    Serial.println("Motion detected  ALARM");
+    digitalWrite(D1, HIGH);
+  }
+
+  delay(1000);
+}
